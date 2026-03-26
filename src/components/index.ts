433ead646baf480a5c0046c7bed756a6a290aedb
hyunseo-han <ols3040015@naver.com>
2026-03-26 22:38:04 +0900

    [feat] compose Hompage and write exports

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+export { default as FadeIn } from "./FadeIn";
+export { default as Nav } from "./Nav";
+export { default as Hero } from "./Hero";
+export { default as About } from "./About";
+export { default as Projects } from "./Projects";
+export { default as ProjectCard } from "./ProjectCard";
+export { default as Skills } from "./Skills";
+export { default as Contact } from "./Contact";
+export { default as Footer } from "./Footer";
---
+++ src/pages/Home.tsx
@@ -0,0 +1,23 @@
+import {
+  Nav,
+  Hero,
+  About,
+  Projects,
+  Skills,
+  Contact,
+  Footer,
+} from "../components";
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <Hero />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
